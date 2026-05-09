6aeeff770783e30ea72062f65eebe81fb4eb1e3f
andy <129526441+hiseeoptic@users.noreply.github.com>
2026-05-09 19:37:02 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    turbopack: false, // Tắt Turbopack, quay về webpack
+  },
 };
 
 export default nextConfig;
